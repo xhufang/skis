126991f687face7bdb8d43bf5e384ad8659f2092
xhu <2665105246@qq.com>
2026-08-14 09:46:00 +0800

    Initial commit

---
+++ src/main/java/io/skis/SkisApplication.java
@@ -0,0 +1,13 @@
+package io.skis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SkisApplication.class, args);
+  }
+
+}
